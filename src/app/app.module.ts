439f46fc6b5a66218cce8aa78e6f0619fa968881
Mateus Pereira <mateus.silva@datainfo.inf.br>
2019-03-25 08:32:01 -0300

    implementação função cadastrar via post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { PainelNegociacaoComponent } from './painel-negociacao/painel-negociacao.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
+import { TableModule } from 'primeng/table';
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelNegociacaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+
+   TableModule,
+   PanelModule,
+   InputTextModule,
+   ButtonModule,
+   ToastModule,
+   
+   
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/painel-negociacao/painel-negociacao.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, enableProdMode } from '@angular/core';
+import { OportunidadeService } from '../oportunidade.service';
+import { MessageService } from 'primeng/api';
+enableProdMode()
+@Component({
+  selector: 'app-painel-negociacao',
+  templateUrl: './painel-negociacao.component.html',
+  styleUrls: ['./painel-negociacao.component.css']
+})
+export class PainelNegociacaoComponent implements OnInit {
+ 
+  oportunidade = {}
+  oportunidades = []
+  
+  constructor(private oportunidadeService: OportunidadeService,
+              private messageService: MessageService
+  ) { 
+    
+  }
+
+  ngOnInit() {
+    this.consultar();
+  }
+
+  consultar() {
+    this.oportunidadeService.listar()
+      .subscribe(resposta=>{
+        this.oportunidades = <any> resposta;
+      });
+  }
+
+  adicionar() {
+    this.oportunidadeService.adicionar(this.oportunidade)
+      .subscribe(()=>{
+        this.oportunidade = {};
+        this.consultar();
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Oportunidade adicionada com sucesso'
+        });
+      })
+  }
+
+}
